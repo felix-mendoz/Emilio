47cfbfa37fb802059ac84a3df4136975dc19543b
Félix <felixmendoza1234@outlook.es>
2025-08-08 10:14:01 -0400

    Agregado fecha de entrega de tarea.

--- backend/src/controllers/tarea.controller.ts
+++ backend/src/controllers/tarea.controller.ts
@@ -4,13 +4,14 @@ import { Tarea, TareaModel } from "../models/tareas.models";
 export const postTarea = async (req: Request, res: Response) => {
   try {
 
-    const {titulo, descripcion, id_materia, estado} = req.body;
+    const {titulo, descripcion, id_materia, estado, fecha_entrega} = req.body;
 
     const Tarea: Omit<Tarea, 'id_tarea'> = {
       id_materia: id_materia,
       titulo: titulo,
       descripcion: descripcion,
-      estado: estado
+      estado: estado,
+      fecha_entrega : fecha_entrega
     };
 
     const nuevaTarea = await TareaModel.create(Tarea);
